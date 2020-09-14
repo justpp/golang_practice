a76c2a8c5d730919f58df4fbfd483ce98407dd9c
yl <1195455487@qq.com>
2020-09-14 11:26:13 +0800

    giao.exe

--- main.go
+++ main.go
@@ -1,17 +1,14 @@
 package main
 
-import (
-	"fmt"
-	"os/exec"
-)
+import "giao/util"
 
 func main() {
 	//util.Manger()
 	//fmt.Println("23")
 	//util.UseFunc(util.Closure(1,3))
 	//util.ReadFileByOs("./nginx.conf")
-	ls := exec.Command("ls", "-a")
-	fmt.Println(ls)
+
 	//value := util.ReadIniByBuf("./application.ini", "common", "database.config.dbname")
 	//fmt.Println(value)
+	util.WritSomethingToFile("g")
 }
