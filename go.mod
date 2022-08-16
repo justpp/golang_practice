aee75b343d30d37c7261ab6fca4519f81e45eea0
justpp <root@localhost.localdomain>
2022-08-16 17:01:24 +0800

    blog logger

--- go.mod
+++ go.mod
@@ -26,4 +26,5 @@ require (
 	golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa // indirect
 	golang.org/x/sys v0.0.0-20220811171246-fbc7d0a398ab // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
 )
---
+++ tour/blog/pkg/logger/logger.go
@@ -0,0 +1,103 @@
+package logger
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"runtime"
+)
+
+type Level int8
+
+type Fields map[string]interface{}
+
+const (
+	LevelDebug Level = iota
+	LevelInfo
+	LevelWarn
+	LevelError
+	LevelFatal
+	LevelPanic
+)
+
+func (l Level) String() string {
+	switch l {
+	case LevelDebug:
+		return "debug"
+	case LevelInfo:
+		return "info"
+	case LevelWarn:
+		return "warn"
+	case LevelError:
+		return "error"
+	case LevelFatal:
+		return "fatal"
+	case LevelPanic:
+		return "panic"
+	}
+	return ""
+}
+
+type Logger struct {
+	newLogger *log.Logger
+	ctx       context.Context
+	fields    Fields
+	callers   []string
+}
+
+func NewLogger(w io.Writer, prefix string, flag int) *Logger {
+	l := log.New(w, prefix, flag)
+	return &Logger{newLogger: l}
+}
+
+func (l *Logger) clone() *Logger {
+	nl := *l
+	return &nl
+}
+
+func (l *Logger) WithFields(f Fields) *Logger {
+	ll := l.clone()
+	if ll.fields == nil {
+		ll.fields = make(Fields)
+	}
+	for k, v := range f {
+		ll.fields[k] = v
+	}
+	return ll
+}
+
+func (l *Logger) WithContext(ctx context.Context) *Logger {
+	ll := l.clone()
+	ll.ctx = ctx
+	return ll
+}
+
+func (l *Logger) WithCaller(skip int) *Logger {
+	ll := l.clone()
+	pc, file, line, ok := runtime.Caller(skip)
+	if ok {
+		f := runtime.FuncForPC(pc)
+		ll.callers = []string{fmt.Sprintf("%s: %d %s", file, line, f.Name())}
+	}
+
+	return ll
+}
+
+func (l *Logger) WithCallersFrames() *Logger {
+	maxCallerDepth := 25
+	minCallerDepth := 1
+	var callers []string
+	pcs := make([]uintptr, maxCallerDepth)
+	depth := runtime.Callers(minCallerDepth, pcs)
+	frames := runtime.CallersFrames(pcs[:depth])
+	for frame, more := frames.Next(); more; frame, more = frames.Next() {
+		callers = append(callers, fmt.Sprintf("%s: %d %s", frame.File, frame.Line, frame.Function))
+		if !more {
+			break
+		}
+	}
+	ll := l.clone()
+	ll.callers = callers
+	return ll
+}
