91f056423aa45c5c430df1d2d90d9f955f99b179
justpp <root@localhost.localdomain>
2022-08-16 16:50:00 +0800

    blog config gorm

--- tour/blog/internal/model/model.go
+++ tour/blog/internal/model/model.go
@@ -1,5 +1,13 @@
 package model
 
+import (
+	"fmt"
+	"giao/tour/blog/global"
+	"giao/tour/blog/pkg/setting"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
 type Model struct {
 	ID         uint32 `gorm:"primary_key" json:"id"`
 	CreatedBy  string `json:"created_by"`
@@ -8,4 +16,26 @@ type Model struct {
 	ModifiedOn uint32 `json:"modified_on"`
 	DeletedOn  uint32 `json:"deleted_on"`
 	IsDel      uint8  `json:"is_del"`
+}
+
+func NewDBEngine(settings *setting.DatabaseSettings) (*gorm.DB, error) {
+	s := "%s:%s@tcp(%s)/%s?charset=%s&parseTime=%t&loc=local"
+	db, err := gorm.Open(settings.DBType, fmt.Sprintf(s,
+		settings.UserName,
+		settings.Password,
+		settings.Host,
+		settings.DBName,
+		settings.Charset,
+		settings.ParseTime,
+	))
+	if err != nil {
+		return nil, err
+	}
+	if global.ServerSetting.RunMode == "debug" {
+		db.LogMode(true)
+	}
+	db.SingularTable(true)
+	db.DB().SetMaxIdleConns(global.DatabaseSetting.MaxIdleConnects)
+	db.DB().SetMaxOpenConns(global.DatabaseSetting.MaxOpenConnects)
+	return db, nil
 }
--- tour/blog/main.go
+++ tour/blog/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"giao/tour/blog/global"
+	"giao/tour/blog/internal/model"
 	"giao/tour/blog/internal/routers"
 	"giao/tour/blog/pkg/setting"
 	"github.com/gin-gonic/gin"
@@ -14,6 +15,11 @@ func init() {
 	err := setupSetting()
 	if err != nil {
 		log.Printf("init.setupSetting err:%v", err)
+	}
+
+	err = setupDBEngine()
+	if err != nil {
+		return
 	}
 }
 
@@ -55,4 +61,15 @@ func setupSetting() error {
 	global.ServerSetting.ReadTimeout *= time.Second
 	global.ServerSetting.WriteTimeout *= time.Second
 	return nil
+}
+
+func setupDBEngine() error {
+	var err error
+	// 这里不能写成 :=
+	// := 会声明新变量 那么全局global.DBEngine != global.DBEngine
+	global.DBEngine, err = model.NewDBEngine(global.DatabaseSetting)
+	if err != nil {
+		return err
+	}
+	return err
 }
