05518f10133e82208f7675fc976e4587770a3733
justpp <shenhao@114study.com>
2022-12-07 17:23:24 +0800

    practice wx

--- wx/wx.go
+++ wx/wx.go
@@ -3,10 +3,10 @@ package wx
 import (
 	"crypto/sha1"
 	"fmt"
+	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
 	"io"
 	"log"
-	"net/http"
 	"sort"
 	"strings"
 )
@@ -19,13 +19,28 @@ func init() {
 }
 
 func WxServer() {
+	router := gin.Default()
+	//router.LoadHTMLGlob("templates/*")
+	//router.LoadHTMLFiles("templates/template1.html", "templates/template2.html")
 	log.Println("Wechat Service: Start!")
-	http.HandleFunc("/", procSignature)
-	err := http.ListenAndServe(":80", nil)
-	if err != nil {
-		log.Println("Wechat Service: ListenAndServe Error: ", err)
-	}
+	router.GET("/check", WxCheckSignature)
+
+	router.Run(":80")
 	log.Println("Wechat Service: Stop!")
+}
+
+func WxCheckSignature(c *gin.Context) {
+	timestamp := c.Query("timestamp")
+	nonce := c.Query("nonce")
+	signature := c.Query("signature")
+	echostr := c.Query("echostr")
+	signatureGen := makeSignature(timestamp, nonce)
+	if signatureGen != signature {
+		log.Println("微信公众号接入失败")
+		return
+	}
+	log.Println("微信公众号接入成功")
+	_, _ = c.Writer.WriteString(echostr)
 }
 
 func makeSignature(timestamp, nonce string) string { //本地计算signature
@@ -35,30 +50,6 @@ func makeSignature(timestamp, nonce string) string { //本地计算signature
 	s := sha1.New()             //返回一个新的使用SHA1校验的hash.Hash接口
 	io.WriteString(s, str)      //WriteString函数将字符串数组str中的内容写入到s中
 	return fmt.Sprintf("%x", s.Sum(nil))
-}
-
-func validateUrl(w http.ResponseWriter, r *http.Request) bool {
-	timestamp := strings.Join(r.Form["timestamp"], "")
-	nonce := strings.Join(r.Form["nonce"], "")
-	signature := strings.Join(r.Form["signature"], "")
-	echostr := strings.Join(r.Form["echostr"], "")
-	signatureGen := makeSignature(timestamp, nonce)
-
-	if signatureGen != signature {
-		return false
-	}
-	fmt.Fprintf(w, echostr) //原样返回eechostr给微信服务器
-	return true
-}
-
-func procSignature(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm() //Request需要解析
-	if !validateUrl(w, r) {
-		fmt.Println("Wechat Service: This http request is not from wechat platform")
-		log.Println("Wechat Service: This http request is not from wechat platform")
-		return
-	}
-	log.Println("validateUrl Ok")
 }
 
 func getConfig() *viper.Viper {
