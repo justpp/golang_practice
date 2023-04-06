d23a323465eeba6480782c2ab75e5f61656a14e8
justpp <shenhao@114study.com>
2023-04-06 09:21:10 +0800

    rpc demo & wx 公众号接入gpt简易版

---
+++ tour/tag_service/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"giao/tour/tag_service/proto"
+	"giao/tour/tag_service/server"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"log"
+	"net"
+)
+
+func main() {
+	s := grpc.NewServer()
+	proto.RegisterTagServiceServer(s, server.NewTagServe())
+
+	reflection.Register(s)
+
+	lis, err := net.Listen("tcp", ":9991")
+	if err != nil {
+		log.Fatalf("net listen err:%s", err)
+	}
+
+	log.Println("serve start")
+	err = s.Serve(lis)
+	if err != nil {
+		log.Fatalf("server.Serve err: %s", err)
+	}
+}
--- wx/config.go
+++ wx/config.go
@@ -1,4 +1,4 @@
-package wx
+package main
 
 import (
 	"crypto/sha1"
@@ -10,15 +10,16 @@ import (
 )
 
 var (
-	token     = ""
-	appId     = ""
-	appSecret = ""
+	token      = ""
+	appId      = ""
+	appSecret  = ""
+	talkSecret = ""
 )
 
 func getConfig() *viper.Viper {
 	vp := viper.New()
 	vp.SetConfigName("env")
-	vp.AddConfigPath("wx/")
+	vp.AddConfigPath("./")
 	vp.SetConfigType("yaml")
 	err := vp.ReadInConfig()
 	if err != nil {
@@ -32,13 +33,14 @@ func setEnv() {
 	token, _ = vp.Get("token").(string)
 	appId, _ = vp.Get("appId").(string)
 	appSecret, _ = vp.Get("appSecret").(string)
+	talkSecret, _ = vp.Get("talkSecret").(string)
 }
 
-func makeSignature(timestamp, nonce string) string { //本地计算signature
+func makeSignature(timestamp, nonce string) string { // 本地计算signature
 	si := []string{token, timestamp, nonce}
-	sort.Strings(si)            //字典序排序
-	str := strings.Join(si, "") //组合字符串
-	s := sha1.New()             //返回一个新的使用SHA1校验的hash.Hash接口
-	io.WriteString(s, str)      //WriteString函数将字符串数组str中的内容写入到s中
+	sort.Strings(si)            // 字典序排序
+	str := strings.Join(si, "") // 组合字符串
+	s := sha1.New()             // 返回一个新的使用SHA1校验的hash.Hash接口
+	io.WriteString(s, str)      // WriteString函数将字符串数组str中的内容写入到s中
 	return fmt.Sprintf("%x", s.Sum(nil))
 }
